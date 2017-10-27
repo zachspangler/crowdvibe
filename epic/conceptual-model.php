73fe657475d713001f4fd6cee00ead51dd3df2de
Zach Spangler <zaspangler@gmail.com>
2017-10-27 11:07:37 -0600

    created file system for the project

---
+++ epic/conceptual-model.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>SnapFlix | Conceptual Model</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
---
+++ epic/use-cases.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>SnapFlix | Use Cases</title>
+	</head>
+	<body>
+		
+	</body>
+</html>
