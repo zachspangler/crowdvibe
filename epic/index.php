48a5c04e1c3b245cf9e40034213b3d990c2cb5f1
Zach Spangler <zaspangler@gmail.com>
2017-10-30 13:44:25 -0600

    added persona number 2

--- epic/index.php
+++ epic/index.php
@@ -7,7 +7,7 @@
 	<body>
 		<a href="conceptual-model.php">Conceptual Model</a>
 		<a href="persona-one-guy.php">Persona 1 - Guy Cleve</a>
-		<a href="persona-one-ziad.php">Persona 2 - </a>
+		<a href="persona-two-devin.php">Persona 2 - Devin Cherokee</a>
 		<a href="persona-one-guy.php">Use Cases</a>
 	</body>
 </html>
