09452a40576447b2c59f118070ae81017b4d6054
luthermckeiver <leoluther96@gmail.com>
2017-10-30 16:15:38 -0600

    Created Persona for Devin Cherokee.

--- epic/persona-one-guy.php
+++ epic/persona-one-guy.php
@@ -6,13 +6,26 @@
 	</head>
 	<body>
 		<h1>Persona</h1>
-		<p><strong>Name:</strong></p>
-		<p><strong>Age:</strong></p>
-		<p><strong>Profession:</strong></p>
-		<p><strong>Technology:</strong></p>
+        <img src="http://www.besteducationnews.com/wp-content/uploads/2014/03/f92a4__sad-male-student-lonely-l-016.jpg" alt="lonely devin"
+             <p></p>
+		<p><strong>Name:</strong> Devin Cherokee</p>
+		<p><strong>Age:</strong> 21</p>
+		<p><strong>Profession:</strong> Biology Major at University of New Mexico</p>
+		<p><strong>Technology:</strong> Uses an Iphone 7 and a Macbook Pro</p>
 		<p><strong>Attitudes and Behaviors:</strong></p>
 		<p><strong>Frustrations and Needs:</strong></p>
-		<p><strong>Goals:</strong></p>
-		<p><strong>User Story:</strong></p>
+        <ul>
+            <li> To find a easy way to find events </li>
+            <li> Very user friendly interface</li>
+            <li> Lack of knowledge of events near him </li>
+            <li> Nobody to hang out with  </li>
+            <li> Lack of time</li>
+            <li> Lack of people near him with similar interest </li>
+        </ul>
+		<p><strong>Goals:</strong> He wants to meet new people and expand his social life </p>
+		<p><strong>User Story:</strong> Devin is a sophomore in college and currently attends the University Of New Mexico. He is a biology major and his life is consumed with studying and labs. Lately he has found himself dissatisfied with his social life. He doesn't go out a lot and is not engaged in a lot of hobbies, so his friend group is limited to the people that he meets in class.
+
+            He is tired of not having anything to do and wants to find a way to go to events, so he can meet new people and have fun.
+        </p>
 	</body>
 </html>
