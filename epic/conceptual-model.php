9bc06abcfc8d36b2fe27fd698d138ba37ecdcdd9
Zach Spangler <zaspangler@gmail.com>
2017-10-30 14:27:03 -0600

    added additional classes to the conceptual model (friend and event)

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -26,29 +26,42 @@
 			<li>eventDuration</li>
 			<li>eventImage</li>
 			<li>eventProfileId (foreign key) - host of event </li>
+			<li>eventType (is this public or private)</li>
 		</ul>
-		<h3>PROFILE RATING</h3>
+		<h3>ATTENDEE RATING</h3>
 		<ul>
-			<li>ratingId (primary key)</li>
-			<li>ratingProfileId (foreign key) - For the person giving the rating</li>
-			<li>ratingProfileId (foreign key) - For the person receiving the rating</li>
-			<li>ratingEventId (foreign key)</li>
-			<li>ratingSocialScore</li>
-			<li>ratingFlakeFactor</li>
-		</ul>
-		<h3>EVENT RATING</h3>
-		<ul>
-			<li>eventRatingId (primary key)</li>
-			<li>ratingProfileId (foreign key) - For the person giving the rating</li>
-			<li>ratingEventId (foreign key)</li>
-			<li>ratingEventScore</li>
+			<li>attendeeRatingId (primary key)</li>
+			<li>attendeeProfileId (foreign key) - For the person giving the rating</li>
+			<li>attendeeProfileId (foreign key) - For the person receiving the rating</li>
+			<li>attendeeEventId (foreign key) - What event the profile is being rated from</li>
+			<li>attendeeSocialScore</li>
+			<li>attendeeFlakeFactor</li>
 		</ul>
 		<h3>EVENT RATING</h3>
 		<ul>
 			<li>eventRatingId (primary key)</li>
-			<li>ratingProfileId (foreign key) - For the person giving the rating</li>
-			<li>ratingEventId (foreign key)</li>
-			<li>ratingEventScore</li>
+			<li>eventRatingProfileId (foreign key) - For the person giving the rating</li>
+			<li>eventRatingEventId (foreign key)</li>
+			<li>eventRatingEventScore</li>
+			<li>eventRatingClosed</li>
+		</ul>
+		<h3>EVENT ATTENDANCE</h3>
+		<ul>
+			<li>attendanceID (primary key)</li>
+			<li>attendanceEventId (foreign key)</li>
+			<li>attendanceProfileId (foreign key)</li>
+			<li>attendanceStatus</li>
+			<li>attendanceNumberAttending</li>
+			<li>attendanceActivationToken</li>
+			<li>attendanceCheckIn</li>
+		</ul>
+		<h3>FRIENDS</h3>
+		<ul>
+			<li>friendID (primary key)</li>
+			<li>friendProfileId (foreign key) - this is the root friend</li>
+			<li>friendProfileId (foreign key) - this is the profile of the friend of the user</li>
+			<li>friendGroupName</li>
+			<li>friendActivationToken</li>
 		</ul>
 	</body>
 </html>
