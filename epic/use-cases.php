6f8d6de0b21fd547f5706dfa53bfba54d0df857c
Matt David <mcdav3636@gmail.com>
2017-10-30 15:49:29 -0600

    create use case

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -5,6 +5,66 @@
 		<title>Let's Chill | Use Cases</title>
 	</head>
 	<body>
-		
-	</body>
+    <h2>Event Creator</h2>
+		<p>Its Friday night and Guy is excited about kicking off the weekend. Guy likes being the coolest dude that
+            throws the hottest events and wants people to talk about it. He uses the __________ app to post his event
+            so he can get the word out to his friends and the public. After accepting the invitation, attendees will be
+            able to live rate his event. Guys goal is to be a "10" whos events reach "on fire" every time.</p>
+
+    <h2>Useage preconditions-</h2>
+    <p>Guy has created a profile with a user name, email, phone, and rating status. He has also "friended" his close
+    chums. He has downloaded the app.</p>
+
+    <h2>Useage postconditions</h2>
+    <p>Guy has created an event that was attended and rated by those who checked in.</p>
+
+    <h2>Interaction flow</h2>
+    <ol>
+        <li>Guy clicks on his app icon</li>
+        <li>app returns home screen</li>
+        <li>Guy clicks the create even button</li>
+        <li>app returns input form</li>
+        <li>Guy enters credentials for even (date/time/location/description/public or friends) and hits submit</li>
+        <li>app returns even created!</li>
+        <li>if event is set to public, event will be posted to event board.</li>
+        <li> Friends will be sent notification any time an even is posted by a friend.</li>
+    </ol>
+
+    <h2>Attendee</h2>
+    <p>Devin is pumped its Friday! He has been under the gun at work all week. He hasn't talked to any of the boys since
+    last weeks schinanigans. He gets home later than most and opens up his _________ app to see whats happening tonight.
+    Devin doesn't want to gamble on his only night out so he want to check out the status of the options in front of
+    him</p>
+
+    <h2>Useage preconditions</h2>
+    <p>Devin has downloaded the app. he has set up his profile and added friends</p>
+
+    <h2>Useage postconditions</h2>
+    <p>Devin has selected an event, rsvp, checked in, and rated the event</p>
+
+    <h2>Interaction Flow</h2>
+    <ol>
+        <li>Devin opens his app</li>
+        <li>app returns home page with tonights options to browse</li>
+        <li>Devin scrolls through events until he finds what he likes.</li>
+        <li>Devin clicks rsvp</li>
+        <li>app returns are you sure?</li>
+        <li>Devin confirms his rsvp</li>
+        <li>app confirms his rsvp and places the even on his itinerary</li>
+        <li>Devin finishes his beer, gets the three s's out of the way. gets dressed and makes his way to the event.
+            he opens his app when he gets there, and clicks the event on his itinerary. </li>
+        <li>app returns have you arrived?</li>
+        <li>Devin clicks to check in.</li>
+        <li>app returns event page with rating input options</li>
+        <li>Devin gets 5 beers in and decides this even is awesome</li>
+        <li>Devin rates the event an 8 and clicks submit</li>
+        <li>App confirms his rating</li>
+        <li>Devin gets 8 beers in and decides he has beef with the event host. He clicks on the creator profile</li>
+        <li>app returns creators profile</li>
+        <li>Devin rates him a 5 because he grew a little drunken compassion.</li>
+        <li>app confirms rating</li>
+        <li>Devin gets the spins, passes out in a chair, and wakes up in his clothes on his couch with his own
+        rating down 3 points.</li>
+    </ol>
+    </body>
 </html>
