335a7346bdce965923ea37f83b2de612869a5836
Zach Spangler <zaspangler@gmail.com>
2017-10-31 10:07:34 -0600

    edited persona one - Guy

--- epic/persona-one-guy.php
+++ epic/persona-one-guy.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>CrowdVibe | Persona Two</title>
-</head>
-<body>
-<h1>Persona</h1>
-<img src="http://thefraternityadvisor.com/wp-content/uploads/2012/08/fraternity-nerd.jpg" alt="Guy Cleve"
-<div>
-
-<p><strong>Name:</strong> Guy Cleve</p>
-<p><strong>Age:</strong> 25</p>
-<p><strong>Profession:</strong> Works at Walgreens as a bagger, student at New Mexico State University.</p>
-<p><strong>Technology:</strong> Has a surface laptop, and Google Pixel.</p>
-<p><strong>Attitudes and Behaviors:</strong></p>
-<p><strong>Frustrations and Needs:</strong></p>
-<ul>
-    <li>Needs an easier way to make friends.</li>
-    <li>He hates unorganized plans, this makes him anxious. </li>
-    <li>He doesn't like complicated apps because of his low attention span.</li>
-    <li>Whenever he makes plans with people, and they dont end up showing up.</li>
-</ul>
-<p><strong>Goals:</strong> The goal of this app for Guy is finally have friends who attend his events so he can actually have fun, spontaneous social life that he has always wanted.</p>
-
-<p><strong>User Story: </strong>Guy Cleve is a man with few identifiable characteristics, few friends, a low  attention span, and is completely foreign to social cues. He is a lonely individual with a sticky note description of a love life, yet, he still refuses to give up his search for the "one". Though his skillset is so minor, he is still described as a tenacious person when put into a situation he is comfortable enough to deal with.  He takes an interest in music, though he does not play any, and aspires to be a well known musician once he figures out which instrument that suites his needs. Guy has been looking for a way to expand his friend base with people well educated on the classical music genre.  Even though unsuccessful in finding a way to do that, he still has hope he will come across a site that will allow him to invite a group of people he can be around and relate to. He wants to be able to check the site, post an event that reaches people within a 5 mile radius, and have people be able to contact him and let him know they will be at that event. He would also like be able to see how well the event is going, how many people are there, and if the event is even worth continuing. If there is a cost of admission, he would like the site to notify his attendees along with how much it will be, and how much will they need to bring to enjoy his their time, and/or buy products supplied at the establishment. Guy does not like to buy into popular opinion, but, he would like a way to see other peoples feed back on the host of the meet-up, and the area or establishment in which the meet-up took place. Hopefully he can find a site where all of his needs can be met, and where he can make himself more appetizing and available to the community around him.
-</p>
-</div>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>CrowdVibe | Persona Two</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<img src="http://thefraternityadvisor.com/wp-content/uploads/2012/08/fraternity-nerd.jpg" alt="Guy Cleve"
+		<div>
+			<p><strong>Name:</strong> Guy Cleve</p>
+			<p><strong>Age:</strong> 25</p>
+			<p><strong>Profession:</strong> Works at Smith's as a bagger, student at New Mexico State University.</p>
+			<p><strong>Technology:</strong> Has a surface laptop, and Google Pixel.</p>
+			<p><strong>Frustrations and Needs:</strong></p>
+			<ul>
+				<li>Needs an easier way to make friends.</li>
+				<li>He hates unorganized plans, this makes him anxious.</li>
+				<li>He doesn't like complicated apps because of his low attention span.</li>
+				<li>He hates when he makes plans with people, and they don't end up showing up.</li>
+			</ul>
+			<p><strong>Goals:</strong> The goal of this app for Guy is to finally have friends who attend his events so he
+				can actually have fun, creating the spontaneous social life that he has always wanted.</p>
+			<p><strong>User Story: </strong>Guy Cleve is a man with few identifiable characteristics, few friends, a low attention span, and is completely foreign to social cues. He is a lonely individual with a sticky note description of a love life, yet, he still refuses to give up his search for the "one". Though his skillset is so minor, he is still described as a tenacious person when put into a situation he is comfortable enough to deal with. He takes an interest in music, though he does not play any instruments. He aspires to be a well known musician once he figures out which instrument suites his needs. Guy has been looking for a way to expand his friend base with people how are well educated in the classical music genre. Even though he has been unsuccessful in finding a way to do that, he still has hope he will come across a site that will allow him to invite a group of people he can be around and relate to. He wants to be able to check the site, post an event that reaches people within a 5 mile radius, and have people be able to contact him and let him know they will be at that event. He would also like be able to see how well the event is going, how many people are there, and if the event is even worth going. If there is a cost of admission, he would like the site to notify his attendees along with how much it will be, and how much will they need to bring to enjoy his their time, and/or buy products supplied at the establishment. Guy does not like to buy into popular opinion, but, he would like a way to see other peoples feedback on the host of the event, and the area or establishment in which the event took place. Hopefully he can find a site where all of his needs can be met, and where he can make himself more appetizing and available to the community around him.</p>
+		</div>
+	</body>
 </html>
