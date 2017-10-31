f6f196a59afb18b4ca52fefbaec33e6f09f1b779
luthermckeiver <leoluther96@gmail.com>
2017-10-30 21:43:25 -0600

    Created Persona for Guy Cleve.

--- epic/persona-one-guy.php
+++ epic/persona-one-guy.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<title>Let's Chill | Persona One</title>
-	</head>
-	<body>
-		<h1>Persona</h1>
-        <img src="http://www.besteducationnews.com/wp-content/uploads/2014/03/f92a4__sad-male-student-lonely-l-016.jpg" alt="lonely devin"
-             <p></p>
-		<p><strong>Name:</strong> Devin Cherokee</p>
-		<p><strong>Age:</strong> 21</p>
-		<p><strong>Profession:</strong> Biology Major at University of New Mexico</p>
-		<p><strong>Technology:</strong> Uses an Iphone 7 and a Macbook Pro</p>
-		<p><strong>Attitudes and Behaviors:</strong></p>
-		<p><strong>Frustrations and Needs:</strong></p>
-        <ul>
-            <li> To find a easy way to find events </li>
-            <li> Very user friendly interface</li>
-            <li> Lack of knowledge of events near him </li>
-            <li> Nobody to hang out with  </li>
-            <li> Lack of time</li>
-            <li> Lack of people near him with similar interest </li>
-        </ul>
-		<p><strong>Goals:</strong> He wants to meet new people and expand his social life </p>
-		<p><strong>User Story:</strong> Devin is a sophomore in college and currently attends the University Of New Mexico. He is a biology major and his life is consumed with studying and labs. Lately he has found himself dissatisfied with his social life. He doesn't go out a lot and is not engaged in a lot of hobbies, so his friend group is limited to the people that he meets in class.
+<head>
+    <meta charset="UTF-8">
+    <title>Let's Chill | Persona Two</title>
+</head>
+<body>
+<h1>Persona</h1>
+<img src="http://thefraternityadvisor.com/wp-content/uploads/2012/08/fraternity-nerd.jpg" alt="Guy Cleve"
+<div>
 
-            He is tired of not having anything to do and wants to find a way to go to events, so he can meet new people and have fun.
-        </p>
-	</body>
+<p><strong>Name:</strong> Guy Cleve</p>
+<p><strong>Age:</strong> 25</p>
+<p><strong>Profession:</strong> Works at Walgreens as a bagger, student at New Mexico State University</p>
+<p><strong>Technology:</strong> Has a surface laptop, and Google Pixel</p>
+<p><strong>Attitudes and Behaviors:</strong></p>
+<p><strong>Frustrations and Needs:</strong></p>
+<ul>
+    <li>Needs an easier way to make friends.</li>
+    <li>He hates unorganized plans, this makes him anxious </li>
+    <li>He doesn't like complicated apps because of his low attention span</li>
+    <li>Whenever he makes plans with people, and they dont end up showing up</li>
+</ul>
+<p><strong>Goals:</strong> The goal of this app for Guy is finally have friends who attend his events so he can actually have fun, spontaneous social life that he has always wanted.</p>
+
+<p><strong>User Story: </strong>Guy Cleve is a man with few identifiable characteristics, few friends, a low  attention span, and is completely foreign to social cues. He is a lonely individual with a sticky note description of a love life, yet, he still refuses to give up his search for the "one". Though his skillset is so minor, he is still described as a tenacious person when put into a situation he is comfortable enough to deal with.  He takes an interest in music, though he does not play any, and aspires to be a well known musician once he figures out which instrument that suites his needs. Guy has been looking for a way to expand his friend base with people well educated on the classical music genre.  Even though unsuccessful in finding a way to do that, he still has hope he will come across a site that will allow him to invite a group of people he can be around and relate to. He wants to be able to check the site, post an event that reaches people within a 5 mile radius, and have people be able to contact him and let him know they will be at that event. He would also like be able to see how well the event is going, how many people are there, and if the event is even worth continuing. If there is a cost of admission, he would like the site to notify his attendees along with how much it will be, and how much will they need to bring to enjoy his their time, and/or buy products supplied at the establishment. Guy does not like to buy into popular opinion, but, he would like a way to see other peoples feed back on the host of the meet-up, and the area or establishment in which the meet-up took place. Hopefully he can find a site where all of his needs can be met, and where he can make himself more appetizing and available to the community around him.
+</p>
+</div>
+</body>
 </html>
