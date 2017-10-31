069feb3d5850d54a4ee70279ec2c63624afe25bc
Zach Spangler <zaspangler@gmail.com>
2017-10-31 09:00:01 -0600

    Added executive summary

--- epic/index.php
+++ epic/index.php
@@ -6,6 +6,8 @@
 	</head>
 	<body>
 		<h1>Directory</h1>
+		<h3>Executive Summary</h3>
+		<p>Our app will content people with similar interests through the posting of near-live events for others in the area to join. The app will have the ability for users to create and attend events. Using a feedback system we will manage the activities of the users. </p>
 		<ul>
 			<li><a href="conceptual-model.php" target="_blank">Conceptual Model</a></li>
 			<li><a href="persona-one-guy.php" target="_blank">Persona 1 - Guy Cleve</a></li>
