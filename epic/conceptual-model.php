239c907485fb9997ee4a2c069d1e5e6fc3c35756
luthermckeiver <leoluther96@gmail.com>
2017-11-03 10:43:27 -0600

    can you see me?

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -51,7 +51,7 @@
 			<li>attendanceEventId (foreign key)</li>
 			<li>attendanceProfileId (foreign key)</li>
 			<li>attendanceCheckIn</li>
-			<li>attendanceNumberAttenddding</li>
+			<li>attendanceNumberAttendding</li>
 		</ul>
 	</body>
 </html>
