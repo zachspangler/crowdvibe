cd2fd049136e91a87acf223059262a256bf0b550
Zach Spangler <zaspangler@gmail.com>
2017-11-06 14:52:40 -0700

    added the autoload.php file

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -29,61 +29,75 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 class Profile extends CrowdVibe {
 	/**
 	 * Profile Id is the unique identifier for a profile; this is a primary key relations
-	 * @var profileId
+	 * @var Uuid $VALID_PROFILE_ID
 	 **/
-	protected $profileId = null;
+	protected $VALID_PROFILE_ID = "PHPUnit test passing";
 
 	/**
 	 * Profile Activation Token is a the way an account is confirmed
-	 * @var profileActivationToken
+	 * @var $VALID_PROFILE_ACTIVATION_TOKEN
 	 **/
-	protected $profileActivationToken = null;
+	protected $VALID_PROFILE_ACTIVATION_TOKEN = "PHPUnit test passing";
 
 	/**
 	 * Profile Bio is the text description for the user
-	 * @var string profileBio
+	 * @var string $VALID_PROFILE_BIO
 	 **/
-	protected $profileBio = null;
+	protected $VALID_PROFILE_BIO = "PHPUnit test passing";
 
 	/**
 	 * Profile Email is the email associated with the profile
-	 * @var string profileEmail
+	 * @var string $VALID_PROFILE_EMAIL
 	 **/
-	protected $profileEmail = null;
+	protected $VALID_PROFILE_EMAIL = "PHPUnit test passing";
 
 	/**
 	 * Profile Fist Name is the first name of the user
-	 * @var string profileFirstName
+	 * @var string $VALID_PROFILE_FIRST_NAME
 	 **/
-	protected $profileFirstName = null;
+	protected $VALID_PROFILE_FIRST_NAME = "PHPUnit test passing";
 
 	/**
 	 * Profile Hash is used for the profile password
-	 * @var profileHash
+	 * @var $VALID_PROFILE_HASH
 	 **/
-	protected $profileHash = null;
+	protected $VALID_PROFILE_HASH;
 
 	/**
 	 * Profile Image is image the profile will use
-	 * @var string profileImage
+	 * @var string $VALID_PROFILE_IMAGE
 	 **/
-	protected $profileImage = null;
+	protected $VALID_PROFILE_IMAGE = "PHPUnit test passing";
 
 	/**
 	 * Profile Last Name is the last name of the user
-	 * @var string profileLastName
+	 * @var string $VALID_PROFILE_LAST_NAME
 	 **/
-	protected $profileLastName = null;
+	protected $VALID_PROFILE_LAST_NAME = "PHPUnit test passing";
 
 	/**
 	 * Profile Salt is used for the profile password
-	 * @var profileSalt
+	 * @var $VALID_PROFILE_SALT
 	 **/
-	protected $profileSalt = null;
+	protected $VALID_PROFILE_SALT;
 
 	/**
 	 * Profile User Name is used as the screen name for profile
-	 * @var string profileUserName
+	 * @var string $VALID_PROFILE_PROFILE_USERNAME
 	 **/
-	protected $profileUserName = null;
+	protected $VALID_PROFILE_PROFILE_USERNAME = "PHPUnit test passing";
+
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp()  : void {
+		// run the default setUp() method first
+		parent::setUp();
+		$password = "abc123";
+		$this->VALID_PROFILE_SALT = bin2hex(random_bytes(32));
+		$this->VALID_PROFILE_HASH = hash_pbkdf2("sha512", $password, $this->VALID_PROFILE_SALT, 262144);
+
+		// create and insert a Profile
+		$this->profile = new Profile(generateUuidV4(), null,"My name is Tester. I love kitty food and puppies.", "test@phpunit.de","Tester",$this->VALID_PROFILE_HASH,"sample_woman", "McTester", $this->VALID_PROFILE_SALT,"test123");
+		$this->profile->insert($this->getPDO());
 }
