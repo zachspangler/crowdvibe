13de97a1005a99bbc88054c11e98afb91aa807ef
Zach Spangler <zaspangler@gmail.com>
2017-11-06 15:42:35 -0700

    updated the Profile class https://app.asana.com/0/464075553520431/467623072621768

--- php/classes/autoload.php
+++ php/classes/autoload.php
@@ -15,7 +15,7 @@ spl_autoload_register(function($class) {
 	 * prefix: the prefix for all the classes (i.e., the namespace)
 	 * baseDir: the base directory for all classes (default = current directory)
 	 **/
-	$prefix = "Edu\\Cnm\\Crowdvibe";
+	$prefix = "Edu\\Cnm\\CrowdVibe";
 	$baseDir = __DIR__;
 	// does the class use the namespace prefix?
 	$len = strlen($prefix);
