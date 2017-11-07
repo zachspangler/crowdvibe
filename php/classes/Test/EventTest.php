4cac282e3372f5f96d50388b5c949be92ae5d185
luthermckeiver <leoluther96@gmail.com>
2017-11-07 12:38:51 -0700

    made some adjustments to Event Class

---
+++ php/classes/Test/EventTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Edu\Cnm\CrowdVibe\Test;
+
+use Edu\Cnm\CrowdVibe\{Profile};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+/**
+ * Full PHPUnit test for the Event class
+ *
+ * This is a complete PHPUnit test of the Event class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see
+ * @author Luther <lmckeiver@cnm.edu>
+ **/
+class Event extends CrowdVibeTest {
+    /**
+     * Profile that created the Event; this is for foreign key relations
+     * @var Profile profile
+     **/
+    protected $profile = null;
+
+
