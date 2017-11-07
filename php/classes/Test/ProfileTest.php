84985450dd24e144a311f3915bd2bc7c24cf1bb2
Zach Spangler <zaspangler@gmail.com>
2017-11-06 22:21:55 -0700

    added unit test for the profile attribute additional unit tests needed https://app.asana.com/0/464883007022852/467623072621768

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zacharyspangler
- * Date: 11/3/17
- * Time: 9:33 AM
- */
-
 namespace Edu\Cnm\CrowdVibe\Test;
 
 use Edu\Cnm\CrowdVibe\{Profile};
@@ -26,18 +19,12 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @author Zach Spangler<zspangler@gmail.com> and Dylan McDonald <dmcdonald21@cnm.edu>
  **/
 
-class Profile extends CrowdVibe {
+class ProfileTest extends CrowdVibeTest {
 	/**
-	 * Profile Id is the unique identifier for a profile; this is a primary key relations
-	 * @var Uuid $VALID_PROFILE_ID
-	 **/
-	protected $VALID_PROFILE_ID = "PHPUnit test passing";
-
-	/**
-	 * Profile Activation Token is a the way an account is confirmed
+	 * Profile Activation Token is a the way an account is confirmed, placeholder until account activation is created
 	 * @var $VALID_PROFILE_ACTIVATION_TOKEN
 	 **/
-	protected $VALID_PROFILE_ACTIVATION_TOKEN = "PHPUnit test passing";
+	protected $VALID_PROFILE_ACTIVATION_TOKEN;
 
 	/**
 	 * Profile Bio is the text description for the user
@@ -49,13 +36,19 @@ class Profile extends CrowdVibe {
 	 * Profile Email is the email associated with the profile
 	 * @var string $VALID_PROFILE_EMAIL
 	 **/
-	protected $VALID_PROFILE_EMAIL = "PHPUnit test passing";
+	protected $VALID_PROFILE_EMAIL1 = "test1@phpunit.de";
+
+	/**
+	 * Profile Email is the email associated with the profile
+	 * @var string $VALID_PROFILE_EMAIL
+	 **/
+	protected $VALID_PROFILE_EMAIL2 = "test2@phpunit.de";
 
 	/**
 	 * Profile Fist Name is the first name of the user
 	 * @var string $VALID_PROFILE_FIRST_NAME
 	 **/
-	protected $VALID_PROFILE_FIRST_NAME = "PHPUnit test passing";
+	protected $VALID_PROFILE_FIRST_NAME = "Ada";
 
 	/**
 	 * Profile Hash is used for the profile password
@@ -73,7 +66,7 @@ class Profile extends CrowdVibe {
 	 * Profile Last Name is the last name of the user
 	 * @var string $VALID_PROFILE_LAST_NAME
 	 **/
-	protected $VALID_PROFILE_LAST_NAME = "PHPUnit test passing";
+	protected $VALID_PROFILE_LAST_NAME = "Lovelace";
 
 	/**
 	 * Profile Salt is used for the profile password
@@ -83,21 +76,120 @@ class Profile extends CrowdVibe {
 
 	/**
 	 * Profile User Name is used as the screen name for profile
-	 * @var string $VALID_PROFILE_PROFILE_USERNAME
+	 * @var string $VALID_PROFILE_USERNAME
 	 **/
-	protected $VALID_PROFILE_PROFILE_USERNAME = "PHPUnit test passing";
+	protected $VALID_PROFILE_USERNAME = "PHPUnit test passing";
 
 	/**
-	 * create dependent objects before running each test
-	 **/
-	public final function setUp()  : void {
-		// run the default setUp() method first
+	 * run the default setup operation to create salt and hash.
+	 */
+	public final function setUp(): void {
 		parent::setUp();
+		//
 		$password = "abc123";
-		$this->VALID_PROFILE_SALT = bin2hex(random_bytes(32));
-		$this->VALID_PROFILE_HASH = hash_pbkdf2("sha512", $password, $this->VALID_PROFILE_SALT, 262144);
+		$this->VALID_SALT = bin2hex(random_bytes(32));
+		$this->VALID_HASH = hash_pbkdf2("sha512", $password, $this->VALID_SALT, 262144);
+		$this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+	}
 
-		// create and insert a Profile
-		$this->profile = new Profile(generateUuidV4(), null,"My name is Tester. I love kitty food and puppies.", "test@phpunit.de","Tester",$this->VALID_PROFILE_HASH,"sample_woman", "McTester", $this->VALID_PROFILE_SALT,"test123");
-		$this->profile->insert($this->getPDO());
+	/**
+	 * test inserting a valid Profile and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidProfile(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+		$profileId = generateUuidV4();
+		$profile = new Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_BIO, $this->VALID_PROFILE_EMAIL1, $this->VALID_PROFILE_FIRST_NAME, $this->VALID_PROFILE_HASH, $this->VALID_PROFILE_IMAGE, $this->VALID_PROFILE_LAST_NAME, $this->VALID_PROFILE_SALT, $this->VALID_PROFILE_USERNAME);
+		$profile->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
+		$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_PROFILE_ACTIVATION_TOKEN);
+		$this->assertEquals($pdoProfile->getProfileBio(), $this->VALID_PROFILE_BIO);
+		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_PROFILE_EMAIL1);
+		$this->assertEquals($pdoProfile->getProfileFirstName(), $this->VALID_PROFILE_FIRST_NAME);
+		$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_PROFILE_HASH);
+		$this->assertEquals($pdoProfile->getProfileImage(), $this->VALID_PROFILE_IMAGE);
+		$this->assertEquals($pdoProfile->getProfileLastName(), $this->VALID_PROFILE_LAST_NAME);
+		$this->assertEquals($pdoProfile->getProfileSalt(), $this->VALID_PROFILE_SALT);
+		$this->assertEquals($pdoProfile->getProfileUserName(), $this->VALID_PROFILE_USERNAME);
+	}
+	/**
+	 * test inserting a Profile, editing it, and then updating it
+	 **/
+	public function testUpdateValidProfile() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+		// create a new Profile and insert to into mySQL
+		$profileId = generateUuidV4();
+		$profile = new Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_BIO, $this->VALID_PROFILE_EMAIL1, $this->VALID_PROFILE_FIRST_NAME, $this->VALID_PROFILE_HASH, $this->VALID_PROFILE_IMAGE, $this->VALID_PROFILE_LAST_NAME, $this->VALID_PROFILE_SALT, $this->VALID_PROFILE_USERNAME);
+		$profile->insert($this->getPDO());
+		// edit the Profile and update it in mySQL
+		$profile->setProfileEmail($this->VALID_PROFILE_EMAIL2);
+		$profile->update($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
+		$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_PROFILE_ACTIVATION_TOKEN);
+		$this->assertEquals($pdoProfile->getProfileBio(), $this->VALID_PROFILE_BIO);
+		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_PROFILE_EMAIL2);
+		$this->assertEquals($pdoProfile->getProfileFirstName(), $this->VALID_PROFILE_FIRST_NAME);
+		$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_PROFILE_HASH);
+		$this->assertEquals($pdoProfile->getProfileImage(), $this->VALID_PROFILE_IMAGE);
+		$this->assertEquals($pdoProfile->getProfileLastName(), $this->VALID_PROFILE_LAST_NAME);
+		$this->assertEquals($pdoProfile->getProfileSalt(), $this->VALID_PROFILE_SALT);
+		$this->assertEquals($pdoProfile->getProfileUserName(), $this->VALID_PROFILE_USERNAME);
+	}
+	/**
+	 * test creating a Profile and then deleting it
+	 **/
+	public function testDeleteValidProfile() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+		$profileId = generateUuidV4();
+		$profile = new Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_BIO, $this->VALID_PROFILE_EMAIL1, $this->VALID_PROFILE_FIRST_NAME, $this->VALID_PROFILE_HASH, $this->VALID_PROFILE_IMAGE, $this->VALID_PROFILE_LAST_NAME, $this->VALID_PROFILE_SALT, $this->VALID_PROFILE_USERNAME);
+		$profile->insert($this->getPDO());
+		// delete the Profile from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$profile->delete($this->getPDO());
+		// grab the data from mySQL and enforce the Profile does not exist
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertNull($pdoProfile);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("profile"));
+	}
+	/**
+	 * test inserting a Profile and regrabbing it from mySQL
+	 **/
+	public function testGetValidProfileByProfileId() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+		$profileId = generateUuidV4();
+		$profile = new Profile($profileId, $this->VALID_PROFILE_ACTIVATION_TOKEN, $this->VALID_PROFILE_BIO, $this->VALID_PROFILE_EMAIL1, $this->VALID_PROFILE_FIRST_NAME, $this->VALID_PROFILE_HASH, $this->VALID_PROFILE_IMAGE, $this->VALID_PROFILE_LAST_NAME, $this->VALID_PROFILE_SALT, $this->VALID_PROFILE_USERNAME);
+		$profile->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
+		$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_PROFILE_ACTIVATION_TOKEN);
+		$this->assertEquals($pdoProfile->getProfileBio(), $this->VALID_PROFILE_BIO);
+		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_PROFILE_EMAIL1);
+		$this->assertEquals($pdoProfile->getProfileFirstName(), $this->VALID_PROFILE_FIRST_NAME);
+		$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_PROFILE_HASH);
+		$this->assertEquals($pdoProfile->getProfileImage(), $this->VALID_PROFILE_IMAGE);
+		$this->assertEquals($pdoProfile->getProfileLastName(), $this->VALID_PROFILE_LAST_NAME);
+		$this->assertEquals($pdoProfile->getProfileSalt(), $this->VALID_PROFILE_SALT);
+		$this->assertEquals($pdoProfile->getProfileUserName(), $this->VALID_PROFILE_USERNAME);
+	}
+	/**
+	 * test grabbing a Profile that does not exist
+	 **/
+	public function testGetInvalidProfileByProfileId() : void {
+		// grab a profile id that exceeds the maximum allowable profile id
+		$fakeProfileId = generateUuidV4();
+		$profile = Profile::getProfileByProfileId($this->getPDO(), $fakeProfileId );
+		$this->assertNull($profile);
+	}
+
 }
