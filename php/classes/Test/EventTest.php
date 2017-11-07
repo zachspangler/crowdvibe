e1b5159ed6a6b182e6bf4608af069620cceaa2f2
luthermckeiver <leoluther96@gmail.com>
2017-11-07 16:58:17 -0700

    made some adjustments to Event Class created EventContent
    https://app.asana.com/0/464883007022852/467623072621766

--- php/classes/Test/EventTest.php
+++ php/classes/Test/EventTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Edu\Cnm\CrowdVibe\Test;
 
-use Edu\Cnm\CrowdVibe\{Profile};
+use Edu\Cnm\CrowdVibe\{Profile, Event};
 
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
@@ -15,7 +15,7 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * This is a complete PHPUnit test of the Event class. It is complete because *ALL* mySQL/PDO enabled methods
  * are tested for both invalid and valid inputs.
  *
- * @see
+ * @see Event
  * @author Luther <lmckeiver@cnm.edu>
  **/
 class Event extends CrowdVibeTest {
@@ -24,5 +24,39 @@ class Event extends CrowdVibeTest {
      * @var Profile profile
      **/
     protected $profile = null;
+
+
+/**
+ * valid profile hash to create the profile object to own the test
+ * @var $VALID_HASH
+ **/
+protected $VALID_PROFILE_HASH;
+
+/**
+ * vaild salt to use to create the profile object to own the test
+ * @var string $VALID_SALT
+ **/
+protected $VALID_PROFILE_SALT;
+
+/**
+ * content of the event
+ * @var string $VALID_EVENTCONTENT
+ **/
+protected $VALID_EVENTCONTENT ="PHPUnit test passing";
+
+/**
+ * content of the updated Event
+ * @var string $VALID_EVENTCONTENT2
+ **/
+protected $VALID_EVENTCONTENT2 = "PHPUnit test still passing";
+
+/**
+ * timestamp of the Event; this starts as null and is assigned later
+ * @var \DateTime $VALID_EVENTDATE
+ **/
+
+}
+
+
 
 
