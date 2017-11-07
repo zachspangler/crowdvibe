9eb98c171ce60873ffb2f046784dd0cb44e1ebd5
Zach Spangler <zaspangler@gmail.com>
2017-11-07 11:42:16 -0700

    added public_html file and api connections

---
+++ public_html/api/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\CrowdVibe\{
+	Profile
+};
+
+/**
+ * api for the Profile class
+ *
+ * @author {Zachary Spangler} <zaspangler@gmail.com>
+ * @coauthor Derek Mauldin <derek.e.mauldin@gmail.com>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
