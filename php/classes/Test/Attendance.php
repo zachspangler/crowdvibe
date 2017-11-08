5dd917aeffc17be95296761f18e781491ac283df
Chris Owens <cowens17@cnm.edu>
2017-11-08 08:48:42 -0800

    created Attendance class

--- php/classes/Test/Attendance.php
+++ php/classes/Test/Attendance.php
@@ -33,4 +33,18 @@ protected  $attendanceCheckIn;
  * records how many people are attending
  */
 protected $attendanceNumberAttending;
+	/**
+	 * constructor for this Comments
+	 *
+	 * @param Uuid $newAttendanceId id of this events or null if a new events
+	 * @param Uuid $newAttendanceProfileId id of the Profile that created the event
+	 * @param Uuid $newAttendanceEventId id of the Event people attend
+	 * @param Uuid $newAttendanceCheckIn how people are going to notify their attendance
+	 * @param string $newAttendanceNumberAttending string containing actual data on the amount of people
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
 }
