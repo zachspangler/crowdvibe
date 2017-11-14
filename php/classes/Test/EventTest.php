7fc34174e4f3fc08dd3dc399c3d18daa27516f3d
luthermckeiver <leoluther96@gmail.com>
2017-11-13 18:11:08 -0700

    will finish tonight.
    https://app.asana.com/0/464883007022852/467623072621766

--- php/classes/Test/EventTest.php
+++ php/classes/Test/EventTest.php
@@ -18,7 +18,159 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @see Event
  * @author Luther <lmckeiver@cnm.edu>
  **/
+class EventTest extends CrowdVibeTest {
+    /**
+     * Profile that created the Event; this is for foreign key relations
+     * @var Profile profile
+     **/
+    protected $profile = null;
 
+
+    /**
+     * valid profile hash to create the profile object to own the test
+     * @var $VALID_HASH
+     **/
+    protected $VALID_PROFILE_HASH;
+
+    /**
+     * valid salt to use to create the profile object to own the test
+     * @var string $VALID_SALT
+     **/
+    protected $VALID_PROFILE_SALT;
+
+    /**
+     * detail of the Event
+     * @var string $VALID_EVENTDETAIL
+     **/
+    protected $VALID_EVENTDETAIL = "PHPUnit test passing";
+
+    /**
+     * content of the updated Event
+     * @var string $VALID_EVENTDETAIL2
+     **/
+    protected $VALID_EVENTDETAIL2 = "PHPUnit test still passing";
+
+    /**
+     * location of event latitude
+     * @var float $VALID_EVENTLAT
+     **/
+    protected $VALID_EVENTLAT;
+
+    /**
+     * location of event longitude
+     * @var float $VALID_EVENTLONG
+     **/
+    protected $VALID_EVENTLONG;
+
+    /**
+     * price of the event
+     * @var float $VALID_EVENTPRICE
+     **/
+    protected $VALID_EVENTPRICE;
+
+    /**
+     * the start of the event
+     * @var /DateTime $VALID_EVENTSTARTDATETIME
+     **/
+    protected $VALID_EVENTSTARTDATETIME;
+
+    /**
+     * the end of the event
+     * @var /DateTime $VALID_EVENTENDDATETIME
+     **/
+    protected $VALID_EVENTENDDATETIME;
+
+    /**
+     * the amount of attendees that can attend an event
+     * @var float $VALID_EVENTATTENDEELIMIT
+     **/
+    protected $VALID_EVENTATTENDEELIMIT;
+
+    /**
+     * this is the image for an event
+     * @var null $VALID_EVENTIMAGE
+     **/
+    protected $VALID_EVENTIMAGE;
+
+    /**
+     * this is the name of the event
+     * @var string $VALID_EVENTNAME
+     **/
+    protected $VALID_EVENTNAME;
+
+    /**
+     * create dependant object before running each test
+     **/
+    public final function setUp() : void {
+        //run the default setUp() method first
+        parent::setUp();
+        $password ="Idkwhatimdoing";
+        $this->VALID_PROFILE_SALT = bin2hex(random_bytes(32));
+        $this->VALID_PROFILE_HASH = hash_pbkdf2("sha512", $password, $this->VALID_PROFILE_SALT, 262144);
+
+
+        //create and insert a Profile to own test Event
+        $this->profile = new Profile(generateUuidV4(), null,"hey im cool", "wow@email.com","JeeWilikers","01010101010101010101010101010101","https://www.google.com/search?q=image&rlz=1C5CHFA_enUS729US729&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjGiIeN3rzXAhXmqFQKHbCfAyAQ_AUICigB&biw=1440&bih=723#imgrc=9rzb-Rok9-rBiM:","Coolio","0110101001001010","WowIamtehbest101");
+        $this->profile->insert($this->getPDO());
+
+
+        //calculate the date (just use the time the unit test was setup...)
+        $this->VALID_EVENTSTARTDATETIME = new \DateTime();
+        $this->VALID_EVENTENDDATETIME = new \DateTime();
+
+        //format the eventstartdatetime to use for testing
+        $this->VALID_EVENTSTARTDATETIME = new \DateTime();
+        $this->VALID_EVENTSTARTDATETIME->sub(new \DateInterval("P10D"));
+
+        //format the eventenddatetime to use for testing
+        $this->VALID_EVENTENDDATETIME = new  \DateTime();
+        $this->VALID_EVENTENDDATETIME->add(new \DateInterval("P10D"));
+    }
+
+    /**
+     * test inserting a valid Event and verify that the actual mySQL data matches
+     **/
+    public function testInsertValifEvent() : void {
+        // count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("event");
+
+        // create a new Event and insert to into mySQL
+        $eventId = generateUuidV4();
+        $event = new Event ($eventId, $this->profile->getProfileId(), $this->VALID_EVENTDETAIL, $this->VALID_EVENTSTARTDATETIME, $this->VALID_EVENTATTENDEELIMIT, $this->VALID_EVENTLAT, $this->VALID_EVENTLONG, $this->VALID_EVENTENDDATETIME);
+            $event->insert($this->getPDO());
+
+        //grab the data from mySQL and enforce the fields match our expectations
+        $pdoEvent = Event::getEventByEventId($this->getPDO(), $event->getEventId());
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("event"));
+        $this->assertEquals($pdoEvent->getEventId(), $eventId);
+        $this->assertEquals($pdoEvent->getEventProfileId(),$this->profile->getProfileId());
+        $this->assertEquals($pdoEvent->getEventAttendeeLimit(), $this->VALID_EVENTATTENDEELIMIT);
+        // format the date too seconds since the beginning of time to avoid round off error
+        $this->assertEquals($pdoEvent->getEventEndDateTime()->getTimestamp(),$this->VALID_EVENTENDDATETIME);
+        $this->assertEquals($pdoEvent->getEventDetail(), $this->VALID_EVENTDETAIL);
+        $this->assertEquals($pdoEvent->getEventImage(), $this->VALID_EVENTIMAGE);
+        $this->assertEquals($pdoEvent->getEventLat(), $this->VALID_EVENTLAT);
+        $this->assertEquals($pdoEvent->getEventLong(), $this->VALID_EVENTLONG);
+        $this->assertEquals($pdoEvent->getEventName(), $this->VALID_EVENTNAME);
+        $this->assertEquals($pdoEvent->getEventPrice(), $this->VALID_EVENTPRICE);
+        // format the date too seconds since the beginning of time to avoid round off error
+        $this->assertEquals($pdoEvent->getEventStartDateTime()->getTimestamp(), $this->VALID_EVENTSTARTDATETIME->getTimestamp());
+    }
+
+    /**
+     * test creating a Event and then deleting it
+     **/
+    public function testDeleteValidEvent() : void {
+        // count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("event");
+
+        // create a new Event and insert into mySQL
+        $eventId = generateUuidV4()
+            $event = new Event($eventId, $this->profile->getProfileId(), $this->VALID_EVENTDETAIL, $this->VALID_EVENTDETAIL,  )
+    }
+
+
+}
 
 
 
