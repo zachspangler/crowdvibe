b36e546f60c9aa9754f8a244c564a13e1f6d8cc9
Matt David <mcdav3636@gmail.com>
2017-11-15 16:59:45 -0700

    make mock data   https://app.asana.com/0/464883007022852/list

--- php/lib/mock-data.php
+++ php/lib/mock-data.php
@@ -19,3 +19,14 @@ $HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
 
 $profile = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "hope to figure this out3", "willy1@buller.com", "Willy", $HASH, "imageofimages3", "William", $SALT, "willster");
 $profile->insert($pdo);
+
+$password = "woo shit";
+$SALT = bin2hex(random_bytes(32));
+$HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
+
+$profile2 = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "I'm a big nancy boy", "cantstopme@hotstuff.com", "Chauncy", $HASH, "afullmoon", "lostashell", $SALT, "thelovedoctor");
+$profile2->insert($pdo);
+
+
+$eventId = generateUuidV4();
+$event = new Event($eventId, $profile->getProfileId(),"","2022-11-04 19:45:32.4426", "chris' 10th birthday", "babiesfirstparty", "35.084319", "-106.619781", "get my big boy pants", "$10");
