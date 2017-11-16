8dc3865015fdc2e29b89ca46aa2e6d282fafa46f
Matt David <mcdav3636@gmail.com>
2017-11-16 11:56:06 -0700

    make mock data   https://app.asana.com/0/464883007022852/list

--- php/lib/mock-data.php
+++ php/lib/mock-data.php
@@ -17,6 +17,7 @@ $password = "fucking work";
 $SALT = bin2hex(random_bytes(32));
 $HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
 
+
 $profile = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "hope to figure this out3", "willy1@buller.com", "Willy", $HASH, "imageofimages3", "William", $SALT, "willster");
 $profile->insert($pdo);
 
@@ -29,9 +30,25 @@ $profile2->insert($pdo);
 
 
 $eventId = generateUuidV4();
-$event = new Event($eventId, $profile->getProfileId(),"","2022-11-04 19:45:32.4426", "chris' 10th birthday", "babiesfirstparty", "35.084319", "-106.619781", "get my big boy pants", "$10");
+$event = new Event($eventId, $profile->getProfileId(),100,"chris' 10th birthday", "2022-11-04 19:45:32.4426", "babiesfirstparty", 35.084319, -106.619781, "get my big boy pants", 10, "2022-11-04 16:45:32.4426");
 $event->insert($pdo);
 
+$eventId2 = generateUuidV4();
+$event2 = new Event($eventId2, $profile->getProfileId(),2,"Dylans barn raising party!", "2022-11-22 23:45:32.4426", "Amish old folks", 46.084319, -146.619781, "build my venue", 1000000, "2022-11-22 06:45:32.4426");
+$event2->insert($pdo);
+
 $eventAttendanceId = generateUuidV4();
-$eventAttendance = new eventAttendance($eventAttendanceId, $event->getEventId(), $profile->getProfileId(), "1", "2");
+$eventAttendance = new EventAttendance($eventAttendanceId, $event->getEventId(), $profile->getProfileId(), 1, 2);
 $eventAttendance->insert($pdo);
+
+$eventAttendanceId1 = generateUuidV4();
+$eventAttendance1 = new EventAttendance($eventAttendanceId1, $event->getEventId(), $profile->getProfileId(), 1, 2);
+$eventAttendance1->insert($pdo);
+
+$eventAttendanceId2 = generateUuidV4();
+$eventAttendance2 = new EventAttendance($eventAttendanceId2, $event->getEventId(), $profile->getProfileId(), 1, 2);
+$eventAttendance2->insert($pdo);
+
+$eventAttendanceId3 = generateUuidV4();
+$eventAttendance3 = new EventAttendance($eventAttendanceId3, $event->getEventId(), $profile->getProfileId(), 1, 2);
+$eventAttendance3->insert($pdo);
