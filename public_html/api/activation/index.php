5b3ef403a44d2188e5dc1e35724105d10ac6669c
Zach Spangler <zaspangler@gmail.com>
2017-11-16 15:51:09 -0700

    added GET for activation api https://app.asana.com/0/464883007022852/481628262249785

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -27,3 +27,43 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	//sanitize activation input
 	$activation = filter_input(INPUT_GET, "activation", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	// handle GET request - if id is present, that activation is returned, otherwise all activations are returned
+	if($method === "GET") {
+		//make sure Profile Activation Token is a valid hash
+		if(ctype_xdigit($activation) === false) {
+			throw (new \InvalidArgumentException("Activation token is invalid.", 405));
+		}
+		//check that activation token is correct length
+		if(strlen($activation) !== 32) {
+			throw (new \InvalidArgumentException("Activation token is invalid length", 405));
+		}
+		//set XSRF Cookie
+		setXsrfCookie();
+		$profile = Profile::getProfileByProfileActivationToken($pdo, $activation);
+		if(empty($profile) === true) {
+			throw(new\InvalidArgumentException("No profile for Activation", 404));
+		}
+		//set activation token to null
+		$profile->setProfileActivationToken(null);
+		//update profile
+		$profile->update($pdo);
+		//update reply
+		$reply->message = "Your account has been activated, start vibing!";
+	}
+	else {
+		throw (new\Exception("Invalid HTTP method", 405));
+	}
+	header("Location: ../../");
+	// update reply with exception information
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+//	$reply->trace = $exception->getTraceAsString();
+	//header("Content-type: application/json");
+	echo json_encode($reply);
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+	//$reply->trace = $typeError->getTraceAsString();
+}
