b1c9b539c5352a019f6821c8d8e76efe41f5d2d4
luthermckeiver <leoluther96@gmail.com>
2017-11-27 19:30:17 -0700

    switched order
    https://app.asana.com/0/464883007022852/467623072621766

--- public_html/api/event/index.php
+++ public_html/api/event/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 
 use Edu\Cnm\CrowdVibe\ValidateDate;
