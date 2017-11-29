1c78b86eb520eba0e70e363794eca8dca630af5b
mdavid5 <mcdav3636@gmail.com>
2017-11-28 17:20:42 -0700

    changed .htaccess in event-attendance         https://app.asana.com/0/464075553520381/481628262249789

--- php/lib/mock-data.php
+++ php/lib/mock-data.php
@@ -13,45 +13,45 @@ require_once( "uuid.php");
 
 $pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/crowdvibe.ini");
 
-//$password = "fucking work";
-//$SALT = bin2hex(random_bytes(32));
-//$HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
-//
-//
-//$profile = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "hope to figure this out3", "willy1@buller.com", "Willy", $HASH, "imageofimages3", "William", $SALT, "willster");
-//$profile->insert($pdo);
-//
-//$password2 = "woo shit";
-//$SALT = bin2hex(random_bytes(32));
-//$HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
-//
-//$profile2 = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "I'm a big nancy boy", "cantstopme@hotstuff.com", "Chauncy", $HASH, "afullmoon", "lostashell", $SALT, "thelovedoctor");
-//$profile2->insert($pdo);
-//
-//
-//$eventId = generateUuidV4();
-//$event = new Event($eventId, $profile->getProfileId(),100,"chris' 10th birthday", "2022-11-04 19:45:32.4426", "babiesfirstparty", 35.084319, -106.619781, "get my big boy pants", 10, "2022-11-04 16:45:32.4426");
-//$event->insert($pdo);
-//
-//$eventId2 = generateUuidV4();
-//$event2 = new Event($eventId2, $profile->getProfileId(),2,"Dylans barn raising party!", "2022-11-22 23:45:32.4426", "Amish old folks", 46.084319, -146.619781, "build my venue", 1, "2022-11-22 06:45:32.4426");
-//$event2->insert($pdo);
-//
-//$eventAttendanceId = generateUuidV4();
-//$eventAttendance = new EventAttendance($eventAttendanceId, $event->getEventId(),$profile->getProfileId(), true, 2);
-//$eventAttendance->insert($pdo);
-//
-//$eventAttendanceId1 = generateUuidV4();
-//$eventAttendance1 = new EventAttendance($eventAttendanceId1, $event->getEventId(),$profile2->getProfileId(),  true, 1);
-//$eventAttendance1->insert($pdo);
-//
-//$eventAttendanceId2 = generateUuidV4();
-//$eventAttendance2 = new EventAttendance($eventAttendanceId2, $event->getEventId(),$profile->getProfileId(),  true, 5);
-//$eventAttendance2->insert($pdo);
-//
-//$eventAttendanceId3 = generateUuidV4();
-//$eventAttendance3 = new EventAttendance($eventAttendanceId3, $event->getEventId(),$profile2->getProfileId(), true, 3);
-//$eventAttendance3->insert($pdo);
+$password = "fucking work";
+$SALT = bin2hex(random_bytes(32));
+$HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
+
+
+$profile = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "hope to figure this out3", "willy1@buller.com", "Willy", $HASH, "imageofimages3", "William", $SALT, "willster");
+$profile->insert($pdo);
+
+$password2 = "woo shit";
+$SALT = bin2hex(random_bytes(32));
+$HASH = hash_pbkdf2("sha512", $password, $SALT, 262144);
+
+$profile2 = new Profile(generateUuidV4(),bin2hex(random_bytes(16)), "I'm a big nancy boy", "cantstopme@hotstuff.com", "Chauncy", $HASH, "afullmoon", "lostashell", $SALT, "thelovedoctor");
+$profile2->insert($pdo);
+
+
+$eventId = generateUuidV4();
+$event = new Event($eventId, $profile->getProfileId(),100,"chris' 10th birthday", "2022-11-04 19:45:32.4426", "babiesfirstparty", 35.084319, -106.619781, "get my big boy pants", 10, "2022-11-04 16:45:32.4426");
+$event->insert($pdo);
+
+$eventId2 = generateUuidV4();
+$event2 = new Event($eventId2, $profile->getProfileId(),2,"Dylans barn raising party!", "2022-11-22 23:45:32.4426", "Amish old folks", 46.084319, -146.619781, "build my venue", 1, "2022-11-22 06:45:32.4426");
+$event2->insert($pdo);
+
+$eventAttendanceId = generateUuidV4();
+$eventAttendance = new EventAttendance($eventAttendanceId, $event->getEventId(),$profile->getProfileId(), true, 2);
+$eventAttendance->insert($pdo);
+
+$eventAttendanceId1 = generateUuidV4();
+$eventAttendance1 = new EventAttendance($eventAttendanceId1, $event->getEventId(),$profile2->getProfileId(),  true, 1);
+$eventAttendance1->insert($pdo);
+
+$eventAttendanceId2 = generateUuidV4();
+$eventAttendance2 = new EventAttendance($eventAttendanceId2, $event->getEventId(),$profile->getProfileId(),  true, 5);
+$eventAttendance2->insert($pdo);
+
+$eventAttendanceId3 = generateUuidV4();
+$eventAttendance3 = new EventAttendance($eventAttendanceId3, $event->getEventId(),$profile2->getProfileId(), true, 3);
+$eventAttendance3->insert($pdo);
 
 $ratingId= generateUuidV4();
 $rating = new Rating($ratingId, $eventAttendance->getEventAttendanceEventId(),$profile->getProfileId(), $profile2->getProfileId(),4);
--- public_html/api/event-attendance/index.php
+++ public_html/api/event-attendance/index.php
@@ -22,7 +22,7 @@ $reply->data = null;
 
 try {
 	// grab the mySQL connection
-	$pdo = connectionToEncryptedMySQL("/etc/apache2/capstone-mysql/crowdvibe.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/crowdvibe.ini");
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
