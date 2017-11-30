8b1182a940c6e8d3fd9b1670258aebc16f6ad8bf
Zach Spangler <zaspangler@gmail.com>
2017-11-30 16:11:14 -0700

    added earl-grey file

---
+++ public_html/api/earl-grey/earl-grey.php
@@ -0,0 +1,26 @@
+<?php
+require_once dirname(__DIR__,3 ) .  "/php/lib/xsrf.php";
+require_once dirname(__DIR__,3 ) .  "/php/lib/jwt.php";
+use Edu\Cnm\CrowdVibe;
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//verify the HTTP method being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	// if the HTTP method is head check/start the PHP session and set the XSRF token
+	if($method === "GET") {
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		setXsrfCookie();
+		$reply->message = "tea ready";
+	} else {
+		throw (new \InvalidArgumentException("Attempting to brew coffee with a teapot", 418));
+	}
+} catch(\Exception  | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+// encode and return reply to front end caller
+echo json_encode($reply);
