fb7a128d36136918f78f1d2a7fcf3785836d528c
Zach Spangler <zaspangler@gmail.com>
2017-11-30 00:15:03 -0700

    added sample landing page and homepage

---
+++ static-ui/sample-landingpage.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta name="viewport" content="width = device-width, initial-scale = 1, shrink-to-fit = no"/>
+		<meta name="author" content="Chris, Luther, Matt, Zach"/>
+		<meta name="description"
+				content="CrowdVibe, is a social platform allowing people to connect through near live event posting."/>
+
+		<!-- Bootstrap CSS -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+				integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+				crossorigin="anonymous"/>
+
+		<!-- Font Awesome Link -->
+
+		<script src="https://use.fontawesome.com/46c6e23a21.js"></script>
+
+		<!-- CrowdVibe CSS -->
+		<link rel="stylesheet" type="text/css" href="styles.css"/>
+
+		<!-- jQuery, Popper.js, Bootstrap JS -->
+
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+				  integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+				  integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+				  crossorigin="anonymous"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- CrowdVibe Javascript -->
+
+		<!--Google Fonts -->
+
+		<!-- CrowdVibe Favicon -->
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+		<title>CrowdVibe</title>
+	</head>
+	<body>
+		<header>
+			<main class="m-0 landing-page">
+				<div class="container h-100">
+					<div class="row">
+						<div class="col-md-6 mt-4 align-self-center">
+							<h1 class="text-white">Welcome to CrowdVibe</h1>
+							<h3 class="text-white">Connect with your friends — and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</h3>
+						</div>
+						<div class="col-md-6 mt-4 align-self-center">
+							<div class="bg-light rounded p-2">
+								<h3 class="mb-0 pb-1">Login</h3>
+								<form class="mx-4 mb-1">
+									<input type="text" class="my-1 w-100" name="email" placeholder="email"><br>
+									<input type="text" class="my-1 w-100" name="password" placeholder="password"><br>
+									<input type="submit" class="m-1 btn-outline-primary" value="Login">
+								</form>
+							</div>
+							<div class="p-4"></div>
+							<div class="bg-light rounded p-2 mb-4">
+							<h3 class="mb-0 pb-1">Sign Up</h3>
+								<form class="mx-4 mb-1">
+									<input type="text" class="my-1 w-100" name="firstName" placeholder="First Name"><br>
+									<input type="text" class="my-1 w-100" name="lastName" placeholder="Last Name"><br>
+									<input type="text" class="my-1 w-100" name="userName" placeholder="Username"><br>
+									<input type="text" class="my-1 w-100" name="email" placeholder="Your Email"><br>
+									<textarea class="m-1 w-100 form-control" name="bio" placeholder="Your Bio"></textarea><br>
+									<input type="text" class="m-1 w-100" name="password" placeholder="Your Password"><br>
+									<input type="text" class="m-1 w-100" name=confirmPassword" placeholder="Confirm Password"><br>
+									<input type="submit" class="m-1 btn-primary" value="Create Your Account">
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</header>
+	</body>
