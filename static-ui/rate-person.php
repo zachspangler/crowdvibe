b382b63b0d46ea9e3b18da8d7aea20dac055f326
mdavid5 <mcdav3636@gmail.com>
2017-11-30 23:36:00 -0700

    try to make image work         https://app.asana.com/0/464075553520381/481628262249789

--- static-ui/rate-person.php
+++ static-ui/rate-person.php
@@ -1,40 +1,115 @@
-<?php
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<meta name="viewport" content="width = device-width, initial-scale = 1, shrink-to-fit = no"/>
+		<meta name="author" content="Chris, Luther, Matt, Zach"/>
+		<meta name="description"
+				content="CrowdVibe, is a social platform allowing people to connect through near live event posting."/>
+
+		<!-- Bootstrap CSS -->
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+				integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+				crossorigin="anonymous"/>
+
+		<!-- Font Awesome Link -->
+
+		<script src="https://use.fontawesome.com/46c6e23a21.js"></script>
+		<link rel="stylesheet" href="font-awesome-animation.min.css">
+
+		<!-- CrowdVibe CSS -->
 
 
-$PAGE_TITLE = "Rate Person";
-require_once ("head-utils.php");?>
+		<!-- jQuery, Popper.js, Bootstrap JS -->
 
-<!-- Navigation Bar -->
-<?php require_once ("header.php");?>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
+				  integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"
+				  integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ"
+				  crossorigin="anonymous"></script>
 
-<main class=" bg-light">
-	<div class="container py-3">
-		<div class="row">
-			<div class="col-md-5 align-content-center">
-				<div class="card">
-					<img class="card-img-top" src="/epic/images/lazy-allison.jpg" alt="Card image cap">
-					<div class="card-body">
-						<h4 class="card-title">Allison Mopsqueezer</h4>
-						<h6 class="card-subtitle mb-2 text-muted">Bio</h6>
-						<p class="card-text">
-							Shut the shizzle up a check out this sizzle augue hendrerit accumsizzle. Yo for sure est. Vivamizzle maurizzle mah nizzle, viverra uhuh ... yih!, facilisizzle id, ultrices shiznit, erat. Break yo neck, yall ante sure gangsta we gonna chung faucibizzle break yo neck, yall luctizzle et check out this posuere cubilia Phat; Donizzle dolizzle. Integer uhuh ... yih!. Gangster pharetra blandizzle ghetto. Phat rutrizzle aliquizzle its fo rizzle. Sizzle gangsta. I saw beyonces tizzles and my pizzle went crizzle sem nulla, venenatis nizzle, scelerisque boom shackalack, boom shackalack izzle, funky fresh.
-						</p>
-						<h6 class="card-subtitle mb-2 text-muted">Encounters</h6>
-						<p class="card-text">ABQ Shuffleboard Shakedown</p>
-						<span class="icon-stack icon-4x">
-						 <i class="icon-sign-blank icon-stack-base" style="color:#ddd"></i>
-						 <i class="icon-signal-1"></i>
-						 <i class="icon-signal-2"></i>
-						 <i class="icon-signal-3"></i>
-						 <i class="icon-signal-4 icon-light"></i>
-						 <i class="icon-signal-5 icon-light"></i>
-					  </span>
-						<a href="#!" class="btn btn-primary">Rate Allison</a>
+		<!-- jQuery Form, Additional Methods, Validate -->
+
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+		<!-- CrowdVibe Javascript -->
+
+		<!--Google Fonts -->
+
+		<!-- CrowdVibe Favicon -->
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+		<title>CrowdVibe</title>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+				<a class="navbar-brand" href="#">CrowdVibe</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+						  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse ml-auto" id="navbarSupportedContent">
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+								data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <img
+									class="rounded-circle m-0"
+									src="https://dl.dropboxusercontent.com/s/s05h8qapt3p00kn/face.jpg?dl=0" height="50" width="50">
+							</a>
+							<div class="dropdown-menu mr-auto" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item mr-auto" href="#">Profile</a>
+								<a class="dropdown-item mr-auto" href="#">Create An Event</a>
+								<a class="dropdown-item mr-auto" href="#">Signout</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</header>
+		<main class=" bg-light">
+			<div class="container py-3">
+				<div class="row">
+					<div class="col-md-5 align-content-center">
+						<div class="card">
+							<img class="card-img-top" src="/static-ui/images/linux-logo-small.jpeg" alt="Card image cap" height="300">
+							<div class="card-body">
+								<h4 class="card-title">Allison Mopsqueezer</h4>
+								<h6 class="card-subtitle mb-2 text-muted">Bio</h6>
+								<p class="card-text">
+									Shut the shizzle up a check out this sizzle augue hendrerit accumsizzle. Yo for sure est. Vivamizzle maurizzle mah nizzle, viverra uhuh ... yih!, facilisizzle id, ultrices shiznit, erat. Break yo neck, yall ante sure gangsta we gonna chung faucibizzle break yo neck, yall luctizzle et check out this posuere cubilia Phat; Donizzle dolizzle. Integer uhuh ... yih!. Gangster pharetra blandizzle ghetto. Phat rutrizzle aliquizzle its fo rizzle. Sizzle gangsta. I saw beyonces tizzles and my pizzle went crizzle sem nulla, venenatis nizzle, scelerisque boom shackalack, boom shackalack izzle, funky fresh.
+								</p>
+								<h6 class="card-subtitle mb-2 text-muted">Encounters</h6>
+								<p class="card-text">ABQ Shuffleboard Shakedown</p>
+								<span class="icon-stack icon-4x">
+								 <i class="icon-sign-blank icon-stack-base" style="color:#ddd"></i>
+								 <i class="icon-signal-1"></i>
+								 <i class="icon-signal-2"></i>
+								 <i class="icon-signal-3"></i>
+								 <i class="icon-signal-4 icon-light"></i>
+								 <i class="icon-signal-5 icon-light"></i>
+							  </span>
+								<a href="#!" class="btn btn-primary">Rate Allison</a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</div>
-</main>
+		</main>
 
 
