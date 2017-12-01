f9518b6f036674871d7cee4375e3ea581ec23429
mdavid5 <mcdav3636@gmail.com>
2017-11-30 22:48:54 -0700

    started rate-person         https://app.asana.com/0/464075553520381/481628262249789

---
+++ static-ui/rate-person.php
@@ -0,0 +1,32 @@
+<?php
+
+
+$PAGE_TITLE = "Rate Person";
+require_once ("head-utils.php");?>
+
+<!-- Navigation Bar -->
+<?php require_once ("header.php");?>
+
+<main class=" bg-light">
+	<div class="container py-3">
+		<div class="row">
+			<div class="col-md-5 align-content-center">
+				<div class="card">
+					<img class="card-img-top" src="/images/pathToYourImage.png" alt="Card image cap">
+					<div class="card-body">
+						<h4 class="card-title">Allison Mopsqueezer</h4>
+						<h6 class="card-subtitle mb-2 text-muted">Bio</h6>
+						<p class="card-text">
+							Shut the shizzle up a check out this sizzle augue hendrerit accumsizzle. Yo for sure est. Vivamizzle maurizzle mah nizzle, viverra uhuh ... yih!, facilisizzle id, ultrices shiznit, erat. Break yo neck, yall ante sure gangsta we gonna chung faucibizzle break yo neck, yall luctizzle et check out this posuere cubilia Phat; Donizzle dolizzle. Integer uhuh ... yih!. Gangster pharetra blandizzle ghetto. Phat rutrizzle aliquizzle its fo rizzle. Sizzle gangsta. I saw beyonces tizzles and my pizzle went crizzle sem nulla, venenatis nizzle, scelerisque boom shackalack, boom shackalack izzle, funky fresh.
+						</p>
+						<h6 class="card-subtitle mb-2 text-muted">Encounters</h6>
+						<p class="card-text">ABQ Shuffleboard Shakedown<a fa-fa star</p>
+						<a href="#!" class="btn btn-primary">Rate Allison</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
+
+
