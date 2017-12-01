fc54624de57eeed5385a47447a9dbde7c452b6e9
Chris Owens <cowens17@cnm.edu>
2017-12-01 10:54:51 -0800

    created edit event popup menu https://app.asana.com/0/464883007022852/467623072621767/f

--- static-ui/edit-event.php
+++ static-ui/edit-event.php
@@ -1,7 +1,58 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: zacharyspangler
- * Date: 11/30/17
- * Time: 5:07 PM
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<div class="card" style="width: 25rem;">
+			<img class="card-img-top" src="https://comps.canstockphoto.com/group-of-friends-at-karaoke-party-picture_csp4326671.jpg" alt="Card image cap">
+			<div class="form-group row">
+				<label for="example-text-input" class="col-2 col-form-label">Event</label>
+				<div class="col-10">
+					<input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="exampleTextarea">Event Description</label>
+				<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+			</div>
+			<div class="form-group row">
+				<label for="example-email-input" class="col-2 col-form-label">Attendee Limit</label>
+				<div class="col-10">
+					<input class="form-control" type="email" value="420" id="example-email-input">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="example-url-input" class="col-2 col-form-label">Start</label>
+				<div class="col-10">
+					<input class="form-control" type="url" value="4:20pm" id="example-url-input">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="example-tel-input" class="col-2 col-form-label">End</label>
+				<div class="col-10">
+					<input class="form-control" type="tel" value="8:40" id="example-tel-input">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="example-password-input" class="col-2 col-form-label">Location</label>
+				<div class="col-10">
+					<input class="form-control" type="tel" value=" 3122 Central Ave SE, Albuquerque, NM 87106" id="example-password-input">
+				</div>
+			</div>
+			<div class="form-group row">
+				<label for="example-number-input" class="col-2 col-form-label">Price</label>
+				<div class="col-10">
+					<input class="form-control" type="number" value="10$" id="example-number-input">
+					<div>
+						<button type="submit" class="btn btn-primary">Finish</button>
+					</div>
+				</div>
+			</div>
+			</div>
+	</body>
+</html>
