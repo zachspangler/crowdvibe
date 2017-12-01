0c999f3b2bcd7c1b0b607f8707c0cb2a29412c30
mdavid5 <mcdav3636@gmail.com>
2017-11-30 22:57:53 -0700

    add signal icon         https://app.asana.com/0/464075553520381/481628262249789

--- static-ui/rate-person.php
+++ static-ui/rate-person.php
@@ -20,7 +20,15 @@ require_once ("head-utils.php");?>
 							Shut the shizzle up a check out this sizzle augue hendrerit accumsizzle. Yo for sure est. Vivamizzle maurizzle mah nizzle, viverra uhuh ... yih!, facilisizzle id, ultrices shiznit, erat. Break yo neck, yall ante sure gangsta we gonna chung faucibizzle break yo neck, yall luctizzle et check out this posuere cubilia Phat; Donizzle dolizzle. Integer uhuh ... yih!. Gangster pharetra blandizzle ghetto. Phat rutrizzle aliquizzle its fo rizzle. Sizzle gangsta. I saw beyonces tizzles and my pizzle went crizzle sem nulla, venenatis nizzle, scelerisque boom shackalack, boom shackalack izzle, funky fresh.
 						</p>
 						<h6 class="card-subtitle mb-2 text-muted">Encounters</h6>
-						<p class="card-text">ABQ Shuffleboard Shakedown<a fa-fa star</p>
+						<p class="card-text">ABQ Shuffleboard Shakedown</p>
+						<span class="icon-stack icon-4x">
+						 <i class="icon-sign-blank icon-stack-base" style="color:#ddd"></i>
+						 <i class="icon-signal-1"></i>
+						 <i class="icon-signal-2"></i>
+						 <i class="icon-signal-3"></i>
+						 <i class="icon-signal-4 icon-light"></i>
+						 <i class="icon-signal-5 icon-light"></i>
+					  </span>
 						<a href="#!" class="btn btn-primary">Rate Allison</a>
 					</div>
 				</div>
