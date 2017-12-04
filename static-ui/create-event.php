b8ebef06c6e71f6d70457ded042adbbdc1d4e747
luthermckeiver <leoluther96@gmail.com>
2017-12-04 07:21:29 -0700

    create event
    https://app.asana.com/0/464883007022852/467623072621766

--- static-ui/create-event.php
+++ static-ui/create-event.php
@@ -1,1 +1,66 @@
-<?php
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+		<!--Bootstrap added-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<!--jQuery added -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+
+<!--CSS-->
+		<style>
+			body {
+				margin-top: 20px; background-color: #eeeeee
+			}
+			.social-event {
+				.glyphicon,
+			}
+			.social-event {
+				.fa,
+			}
+			.social-event {
+			[class^=icon-']
+			}
+			.social-event {
+			[class*='icon-']
+			}
+			{
+				font-weight: bold;
+				color: #999999;
+			}
+
+			.social-event a,
+			.social-event a: hover,
+			.social-event a:active,
+			.social-event a: focus; {
+			text-decoration: none;
+			}
+		</style>
+
+
+
+	</head>
+	<body>
+		<div class="col-md-offset-3 col-md-6 col-xs-12">
+			<div class="social-event">
+				<form>
+					<ul class="list-inline" id="createPost">
+						<li class=active><a href='#'>Add Event</a></li>
+						<li class='active'><a href='#'>Add Photo</a></li>
+					</ul>
+					<textarea class="form-control" placeholder="What's your event about?"></textarea>
+					<ul class='list-inline post-actions'>
+						<li><a href="#"><span class="glyphicon glyphicon-camera"></span></a></li>
+						<li><a href="#" class='glyphicon glyphicon-user'></a></li>
+						<li><a href="#" class='glyphicon glyphicon-map-marker'></a></li>
+						<li class='pull-right'><a href="#" class='btn btn-dark btn-xs'>Post</a></li>
+					</ul>
+				</form>
+			</div>
+		</div>
+
+	</body>
+</html>
