593848f63faadfd3c3a673e1be979ce76cc4f22d
Zach Spangler <zaspangler@gmail.com>
2017-12-04 15:43:29 -0700

    added services.ts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+
+
+@Injectable()
+
+export class AuthService {
+
+
+	private token : string = localStorage.getItem("jwt-token");
+
+
+
+	constructor(private jwtHelperService: JwtHelperService, private http: HttpClient) {}
+
+	//token : string = this.jwtHelperService.tokenGetter();
+	loggedIn() {
+
+
+
+		if (this.token) {
+			return false;
+		}
+
+		const tokenExpired: boolean = this.jwtHelperService.isTokenExpired(this.token);
+
+		return !tokenExpired
+	}
+
+	decodeJwt() : any {
+		let isLoggedIn : boolean = this.loggedIn();
+
+		if (!isLoggedIn) {
+			return false;
+		}
+		const authObject = this.jwtHelperService.decodeToken(this.token);
+
+		console.log(authObject);
+
+		return authObject;
+	}
+
+}
---
+++ src/app/services/session.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Status} from "../classes/status";
+
+@Injectable()
+
+
+export class SessionService {
+
+
+	constructor(protected http:HttpClient) {}
+
+	private sessionUrl = "api/earl-grey/";
+
+	setSession() {
+		return (this.http.get<Status>(this.sessionUrl, {}));
+
+	}
+
+}
