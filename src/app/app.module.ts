0dc43ff1cdd48d2685d90f10092dd7019288a3d5
Zach Spangler <zaspangler@gmail.com>
2017-12-04 15:50:36 -0700

    added html and css for the create event module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import {NgModule} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import {JwtModule} from "@auth0/angular-jwt";
+import {Status} from "./classes/status";
+
 
 const moduleDeclarations = [AppComponent];
 
+//configure the parameters fot the JwtModule
+const JwtHelper = JwtModule.forRoot({
+	config: {
+		tokenGetter: () => {
+			return localStorage.getItem("jwt-token");
+		},
+		skipWhenExpired:true,
+		whitelistedDomains: ["localhost:7272", "https://bootcamp-coders.cnm.edu/"],
+		headerName:"X-JWT-TOKEN",
+		authScheme: ""
+	}
+});
+
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, routing],
+	imports:      [BrowserModule, HttpClientModule, JwtHelper,ReactiveFormsModule, FormsModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [...appRoutingProviders]
+	providers:    [appRoutingProviders]
 })
 export class AppModule {}
