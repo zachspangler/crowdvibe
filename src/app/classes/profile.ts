e191ede98604e83a5b0c9c60c8c2f74a8b76cc9f
Zach Spangler <zaspangler@gmail.com>
2017-12-04 11:32:35 -0700

    updated scr profile class

---
+++ src/app/classes/profile.ts
@@ -0,0 +1,3 @@
+export class Profile {
+	constructor(public id: string, public profileActivationToken: string, public profileBio: string, public profileEmail: string, public profileFirstName: string, public profileImage: string, public profileLastName: string, public profileUsername: string) {}
+}
