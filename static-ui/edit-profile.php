fd51dafd1c9bdc41cf558a131124f7223a43dce3
Chris Owens <cowens17@cnm.edu>
2017-12-03 18:25:46 -0800

    celaned up id's on edit profile pop up https://app.asana.com/0/464883007022852/467623072621767/f

--- static-ui/edit-profile.php
+++ static-ui/edit-profile.php
@@ -8,8 +8,8 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<div class="card" style="width: 20rem;">
-			<img class="card-img-top" src="http://image.shutterstock.com/display_pic_with_logo/1020994/174834485/stock-photo-arab-saudi-man-breathing-deep-fresh-air-in-the-beach-with-the-ocean-and-horizon-in-the-background-174834485.jpg" alt="Card image cap">
+		<div class="card bg-dark mb-3" style="max-width: 20rem">
+			<img class="card-img-top" src="http://www.talktocanada.com/wp-content/uploads/2014/02/Fotolia_34730819_S-660x346.jpg" alt="Card image cap">
 			<div class="card-block">
 				<form>
 					<div class="form-group">
@@ -17,26 +17,26 @@
 						<input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp" placeholder="Username">
+						<input type="email" class="form-control" id="changeUsername" aria-describedby="emailHelp" placeholder="Username">
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp" placeholder="First Name">
+						<input type="email" class="form-control" id="changeFirstName" aria-describedby="emailHelp" placeholder="First Name">
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" id="exampleInputUsername" aria-describedby="emailHelp" placeholder="Last Name">
+						<input type="email" class="form-control" id="changeLastName" aria-describedby="emailHelp" placeholder="Last Name">
 					</div>
 					<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						<input type="password" class="form-control" id="changePassword" placeholder="Password">
 					</div>
 					<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirm Password">
+						<input type="password" class="form-control" id="confirmChangePassword" placeholder="Confirm Password">
 					</div>
 					<div class="form-group">
 						<label for="exampleTextarea">Bio</label>
-						<textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+						<textarea class="form-control" id="ChangeBio" rows="3"></textarea>
 					</div>
 
-					<button type="submit" class="btn btn-primary">Submit</button>
+					<button type="submit" class="btn btn-dark">Submit</button>
 				</form>
 			</div>
 		</div>
