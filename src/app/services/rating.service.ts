151c7160d69b8d396dbaee151782b5a21d01af2a
mdavid5 <mcdav3636@gmail.com>
2017-12-06 14:19:22 -0700

    started rating.service       https://app.asana.com/0/464075553520381/481628262249789

--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from "@angular/core";
+
 import {HttpClient} from "@angular/common/http";
 import {Status} from "../classes/status";
 import {Rating} from "../classes/rating";
@@ -8,5 +9,32 @@ import {Observable} from "rxjs/Observable";
 export class RatingService {
 
 	constructor(protected http: HttpClient) {
+
+	}
+
+	//define the API endpoint
+	private ratingUrl = "api/rating";
+
+	//call to the Rating API and get a rating object by its Id
+	getRating(id: string): Observable<Status> {
+		return (this.http.get<Rating>(this.ratingUrl + id));
+	}
+
+	//call to the API to grab an array of ratings based on the user input
+	getRatingByRatingEventAttendanceId(ratingEventAttendanceId: string):
+	Observable<Rating[]> {
+		return (this.http.get<Rating[]>(this.ratingUrl + "?ratingEventAttendanceId=" + ratingEventAttendanceId));
+	}
+
+	//call to the API to grab an array of ratings based on the user input
+	getRatingByRatingRateeProfileId(ratingRateeProfileId: string):
+	Observable<Rating[]> {
+		return (this.http.get<Rating[]>(this.ratingUrl + "?ratingRateeProfileId=" + ratingRateeProfileId));
+	}
+
+	//call to the API to grab an array of ratings based on the user input
+	getRatingByRatingEventId(ratingEventId: string):
+	Observable<Rating[]> {
+		return (this.http.get<Rating[]>(this.ratingUrl + "?ratingEventId=" + ratingEventId));
 	}
 }
