54a198d39a7c0c2bbdd9624a533c5b35d02515e1
luthermckeiver <leoluther96@gmail.com>
2017-12-06 16:24:51 -0700

    added landing page html & css.
    https://app.asana.com/0/464883007022852/467623072621766

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -6,7 +6,33 @@ import {Observable} from "rxjs/Observable";
 
 @Injectable ()
 export class EventService {
-
 	constructor(protected http: HttpClient) {
+
+	}
+
+	// define the API endpoint
+	private eventURL = "api/event/";
+
+	//call out to the event API and delete the event in question.
+	deleteEvent(id: number) : Observable<Status> {
+		return(this.http.delete<Status>(this.eventURL + id))
+
+	}
+
+	// call out to the event API and edit the event in question
+	editEvent(event : Event) : Observable<Status> {
+		return (this.http.put<Status>(this.eventURL + event.id, event));
+	}
+
+	// call to event API and create the event in question
+	createEvent(event : Event) : Observable<Status> {
+		return(this.http.post<Status>(this.eventURL, event));
+	}
+
+	// call to the tweet API and get a tweet object based on its Id
+	getEvent(id : number) : Observable<Event> {
+		return (this.http.get<Event>(this.eventURL + id));
+
 	}
 }
+
