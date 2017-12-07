df8cca58364720fc66a9ac8acff6dbd08a5769fe
mdavid5 <mcdav3636@gmail.com>
2017-12-06 20:24:55 -0700

    started event.component      https://app.asana.com/0/464075553520381/481628262249789

--- src/app/components/event.component.ts
+++ src/app/components/event.component.ts
@@ -1,5 +1,14 @@
 import {Component} from "@angular/core";
 
+import {Router} from "@angular/router";
+import {Observables} from "rxjs/Observables";
+import {Status} from "../classes/status";
+import {EventService} from "../services/event.service";
+import {Event} from "../classes/event";
+import {CookieService} from "ng2-cookies";
+
+declare var $: any;
+
 
 @Component({
 	templateUrl: "./templates/event.html"
