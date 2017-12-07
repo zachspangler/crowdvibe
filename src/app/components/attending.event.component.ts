517028d4edd01bac8b98a53b4df2a07a289305fe
Chris Owens <cowens17@cnm.edu>
2017-12-07 13:21:25 -0800

    finished event attendance component https://app.asana.com/0/464883007022852/467623072621767/f

--- src/app/components/attending.event.component.ts
+++ src/app/components/attending.event.component.ts
@@ -1,9 +1,49 @@
 import {Component} from "@angular/core";
-
+import {Router} from "@angular/router";
+import {Status} from "../classes/status";
+import {EventAttendanceService} from "../services/event.attendance.service";
+import {EventAttendance} from "../classes/eventAttendance";
+import {setTimeout} from "timers";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-	selector: "attending-event",
+	selector: "attending-eventAttendance",
 	templateUrl: "./templates/attending-confirmation.html"
 })
 
-export class AttendingEventComponent {}
+export class EventAttendanceComponent {
+	createEventAttendanceForm: FormGroup;
+	eventAttendance: EventAttendance = new EventAttendance(null,null,null,null,null);
+	status: Status = null;
+
+	constructor(private formBuilder: FormBuilder, private router: Router, private eventAttendanceService: EventAttendanceService){
+		console.log("Attendance Recorded")
+	}
+
+	gOnInit(): void{
+		this.createEventAttendanceForm = this.formBuilder.group({
+			eventAttendanceCheckIn: ["",[Validators.maxLength(500), Validators.required]]
+		});
+	}
+	createEventAttendance(): void{
+
+		let createEventAttendance = CreateEventAttendance(null,null,null, this.createEventAttendanceForm.value.eventAttendanceCheckIn, this.createEventAttendanceForm.value.eventAttendanceNumberAttending);
+
+		this.eventAttendanceService.createEventAttendance(createEventAttendance)
+			.subscribe(status=>{
+				this.status = status;
+
+				if(this.status.status === 500){
+					alert(status.message);
+					setTimeout(function(){
+						$("#createEventAttendance").modal('hide');
+					}, 500);
+					this.router.navigate(["home"])
+				}
+			})
+	}
+
+}
+
+
+
