de1c892f86faf6f18f912140089d18e9cd20b3b4
mdavid5 <mcdav3636@gmail.com>
2017-12-07 21:52:57 -0700

    add class attendance profile      https://app.asana.com/0/464075553520381/481628262249789

---
+++ src/app/classes/attendanceProfiles.ts
@@ -0,0 +1,12 @@
+export class attendanceProfiles {
+    constructor(public profileId: string,
+                public profileActivationToken: string,
+                public profileBio: string,
+                public profileEmail: string,
+                public profileFirstName: string,
+                public profileImage: string,
+                public profileLastName: string,
+                public profileUsername: string,
+                public profileRating: number
+    ){}
+}
