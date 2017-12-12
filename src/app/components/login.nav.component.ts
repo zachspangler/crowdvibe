c6f973d62c0b7eef4696acee7b6807d0d461e713
luthermckeiver <leoluther96@gmail.com>
2017-12-12 16:12:52 -0700

    commented out
    https://app.asana.com/0/464883007022852/467623072621766

--- src/app/components/login.nav.component.ts
+++ src/app/components/login.nav.component.ts
@@ -2,6 +2,8 @@ import {Component} from "@angular/core";
 import {Profile} from "../classes/profile";
 import {ProfileService} from "../services/profile.service";
 import {VOID_VALUE} from "@angular/animations/browser/src/render/transition_animation_engine";
+import {Router} from "@angular/router";
+import {Subject} from 'rxjs/Subject';
 
 
 @Component({
@@ -9,44 +11,51 @@ import {VOID_VALUE} from "@angular/animations/browser/src/render/transition_anim
 	templateUrl: "./templates/login-nav.html"
 })
 
-export class LoginNavComponent {
+export class LoginNavComponent {}
 
-
-//observable used for searching Profiles by name
-	termStream = new Subject<string>();
-
-	profile: Profile = new Profile(null, null, null, null, null, null, null, null);
-
-	constructor(private profileService: ProfileService, private router: Router) {
-		this.termStream
-			.subscribe(term => this.filterProfileByName(term));
-
-	}
-
-	ngOnInit(): void {
-		this.reloadProfile();
-
-	}
-
-	reloadProfile(): void {
-		this.reloadProfile.getAllProfiles()
-			.subscribe(profile => this.profile = profile);
-
-	}
-
-	filterProfileByName(term : string ) : void {
-		this.profileService.getProfileByProfileName(term)
-			.debounceTime(30000)
-			.subscribe(profile => {
-				this.profile = profile;
-				if (this.filteredProfiles !== null) {
-					console.log("I work");
-					console.log(this.profile);
-				}
-			});
-	}
-}
-
-
-
+//
+// //observable used for searching Profiles by name
+// 	termStream = new Subject<string>();
+//
+// 	profile: Profile = new Profile(null, null, null, null, null, null, null, null);
+//
+// 	profiles: Profile[]=[];
+//
+// 	constructor(private profileService: ProfileService, private router: Router) {
+// 		this.termStream
+// 			.subscribe(term => this.filterProfileByName(term));
+//
+// 	}
+//
+// 	ngOnInit(): void {
+// 		this.reloadProfile();
+//
+// 	}
+//
+// 	reloadProfile(): void {
+// 		this.profileService.getProfileByProfileName(this.termStream)
+// 			.subscribe(profile => this.profile = profile);
+//
+// 	}
+// 	reloadApplicationCohorts()	 : void {
+// 		this.applicationCohortService.getAllApplicationCohorts()
+// 			.subscribe(applicationCohorts => this.applicationCohorts = applicationCohorts);
+//
+// 	}
+//
+// 	filterProfileByName(term : string ) : void {
+// 		this.profileService.getProfileByProfileName(term)
+// 			.debounceTime(30000)
+// 			.subscribe(profile => {
+// 				this.profile = profile;
+// 				if (this.filteredProfiles !== null) {
+// 					console.log("I work");
+// 					console.log(this.profile);
+// 				}
+// 			});
+// 	}
+//
+//
+//
+//
 
