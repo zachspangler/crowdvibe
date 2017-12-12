735890f7813910276d420af0faaf8f3338e2b23c
luthermckeiver <leoluther96@gmail.com>
2017-12-12 15:45:36 -0700

    mason is helping out
    https://app.asana.com/0/464883007022852/467623072621766

--- src/app/components/login.nav.component.ts
+++ src/app/components/login.nav.component.ts
@@ -1,5 +1,7 @@
 import {Component} from "@angular/core";
 import {Profile} from "../classes/profile";
+import {ProfileService} from "../services/profile.service";
+import {VOID_VALUE} from "@angular/animations/browser/src/render/transition_animation_engine";
 
 
 @Component({
@@ -7,11 +9,44 @@ import {Profile} from "../classes/profile";
 	templateUrl: "./templates/login-nav.html"
 })
 
-export class LoginNavComponent {}
+export class LoginNavComponent {
+
+
+//observable used for searching Profiles by name
+	termStream = new Subject<string>();
+
+	profile: Profile = new Profile(null, null, null, null, null, null, null, null);
+
+	constructor(private profileService: ProfileService, private router: Router) {
+		this.termStream
+			.subscribe(term => this.filterProfileByName(term));
+
+	}
+
+	ngOnInit(): void {
+		this.reloadProfile();
+
+	}
+
+	reloadProfile(): void {
+		this.reloadProfile.getAllProfiles()
+			.subscribe(profile => this.profile = profile);
+
+	}
+
+	filterProfileByName(term : string ) : void {
+		this.profileService.getProfileByProfileName(term)
+			.debounceTime(30000)
+			.subscribe(profile => {
+				this.profile = profile;
+				if (this.filteredProfiles !== null) {
+					console.log("I work");
+					console.log(this.profile);
+				}
+			});
+	}
+}
 
 
 
-// //observable used for searching Profiles by name
-// termStream = new Subject<string>();
-//
-//  profileUsername : Profile  profile = new Profile(null,null, null, null, null, null, null, null);
+
