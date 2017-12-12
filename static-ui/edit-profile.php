7e4791c55f4d6ee70238e6b6ae661a1751825a4e
Chris Owens <cowens17@cnm.edu>
2017-12-12 15:38:54 -0800

    made made edit profile form

--- static-ui/edit-profile.php
+++ static-ui/edit-profile.php
@@ -2,37 +2,58 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+		<!--Bootstrap added-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<!--jQuery added -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<!--CSS-->
+		 <style>
+			 .btn{
+				 color: #FFA500;
+				 border-color: #FFA500;
+			 }
+			 .form-control{
+				 position: center;
+			 }
+			 .form-control-file{
+				 margin-top: 10px;
+			 }
+		 </style>
 	</head>
 	<body>
-		<div class="card bg-dark mb-3" style="max-width: 20rem">
-			<img class="card-img-top" src="http://www.talktocanada.com/wp-content/uploads/2014/02/Fotolia_34730819_S-660x346.jpg" alt="Card image cap">
+		<div class="btn card bg-dark border-4 border-warning mb-3" style="max-width: 20rem">
+			<img class="card-img-top" src="http://davidsheff.com/wp-content/uploads/snoopdogg300.jpg" alt="Card image cap">
 			<div class="card-block">
 				<form>
 					<div class="form-group">
-						<label for="exampleInputFile">Change Profile Picture</label>
-						<input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp">
+						<input type="file" class="form-control-file" id="pictureInputFile" aria-describedby="fileHelp">
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" id="changeUsername" aria-describedby="emailHelp" placeholder="Username">
+						<label>Change Username</label>
+						<input style="width: 18.3rem" class="form-control" id="changeUsername" aria-describedby="emailHelp" placeholder="Username">
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" id="changeFirstName" aria-describedby="emailHelp" placeholder="First Name">
+						<label>First Name</label>
+						<input style="width: 18.3rem" class="form-control" id="changeFirstName" aria-describedby="emailHelp" placeholder="First Name">
 					</div>
 					<div class="form-group">
-						<input type="email" class="form-control" id="changeLastName" aria-describedby="emailHelp" placeholder="Last Name">
+						<label>Last Name</label>
+						<input style="width: 18.3rem" class="form-control" id="changeLastName" aria-describedby="emailHelp" placeholder="Last Name">
 					</div>
 					<div class="form-group">
-						<input type="password" class="form-control" id="changePassword" placeholder="Password">
+						<label>Change Password</label>
+						<input style="width: 18.3rem" class="form-control" id="changePassword" placeholder="Password">
 					</div>
 					<div class="form-group">
-						<input type="password" class="form-control" id="confirmChangePassword" placeholder="Confirm Password">
+						<label>Confirm Password</label>
+						<input style="width: 18.3rem" class="form-control" id="confirmChangePassword" placeholder="Confirm Password">
 					</div>
 					<div class="form-group">
-						<label for="exampleTextarea">Bio</label>
+						<label for="textarea">Bio</label>
 						<textarea class="form-control" id="ChangeBio" rows="3"></textarea>
 					</div>
 
